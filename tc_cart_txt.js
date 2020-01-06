a8343fbd46a56228a7bc66f7bf2e93bdbcae9b26
Nick Surace <58526354+nsurace@users.noreply.github.com>
2020-01-06 11:03:52 -0500

    initial commit

---
+++ tc_cart_txt.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+
+   Author: Nick 
+   Date:   1/6/2020
+   
+   Filename: tc_cart.js
+	
+*/
+
